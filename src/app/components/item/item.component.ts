e0dd2a1b36b2ca7db36883d2e889048a03546bfd
Ismael Oliveira <ismael6991@live.com>
2023-03-01 22:28:44 -0300

    Alimentado a lista

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,10 +1,11 @@
 import {
   Component,
-  OnInit,
+  EventEmitter,
   Input,
   OnChanges,
+  OnInit,
   Output,
-  EventEmitter,
+  OnDestroy,
 } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
@@ -14,9 +15,10 @@ import { Item } from 'src/app/interfaces/iItem';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css'],
 })
-export class ItemComponent implements OnInit, OnChanges {
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
   @Input() item!: Item;
   @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoIdParaDeletar = new EventEmitter();
 
   faPen = faPen;
   faTrash = faTrash;
@@ -29,5 +31,12 @@ export class ItemComponent implements OnInit, OnChanges {
 
   editarItem() {
     this.emitindoItemParaEditar.emit(this.item);
+  }
+  delItem() {
+    console.log('Não quero te comprar!');
+    this.emitindoIdParaDeletar.emit(this.item.id);
+  }
+  ngOnDestroy() {
+    console.log('Não te comprei!');
   }
 }
